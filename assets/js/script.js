e40e6137e024b8ece1b561887dc1cc073f4a2d02
bpoole53 <bpoole53@gmail.com>
2023-07-16 15:38:13 -0500

    added a timeofday variable then a function to add a past, present, or future class to the time block compared to the timeofday

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,15 @@ $(document).ready(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
-  /* Get input from the user in the text area.  Also Get the id based on the hour of the time-block that is a parent to the save button for that hour. Lastly get the input and put it in local storage using the hour id as the key. */
+  /* add a click listener event to get input from the user in the text area by looking amongst the siblings of saveBtn for 'textarea' and then taking the val of that.  Also Get the id based on the hour of the time-block that is a parent to the save button for that hour. Lastly get the input and put it in local storage using the hour id as the key. */
   $('.saveBtn').click(function() {
     var inputText = $(this).siblings('textarea').val();
     var block = $(this).parent().attr('id');
 
     localStorage.setItem(block, inputText);
   });
+
+  
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -23,6 +25,23 @@ $(document).ready(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   // Get the current hour in 24-hour time using Day.js
+
+  /* declare a variable set to the hour of the day using dayjs.  Then split the id string and take the 2nd index value - so [1] - to get the number.  But since it's still a string value put that within parseInt to convert to an int value.  finally compare each parsed int value to the timeOfDay variable to see which class needs to be added to each time block. */
+  var timeOfDay = dayjs().hour();
+  console.log(timeOfDay)
+
+  $('.time-block').each(function() {
+    var timeBlockId = parseInt($(this).attr('id').split('-')[1]);
+    console.log(timeBlockId);
+
+    if (timeBlockId < timeOfDay) {
+      $(this).addClass('past');
+    } else if (timeBlockId === timeOfDay) {
+      $(this).addClass('present');
+    } else {
+      $(this).addClass('future');
+    }
+  });
   //
   // TODO: Add code to display the current date in the header of the page.
   var currentDate = dayjs().format('dddd, MMMM DD YYYY');
